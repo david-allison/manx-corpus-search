10ebe35685fede83a9d230e5d57c292c0dcf9613
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-07-05 21:14:04 +0100

    feat: endpoint linking to newspaper image
    Extracts the logic from newspaper -> link from our source
    Allows an internal redirect that we can change if required
    
    Related: #54 #52

---
+++ CorpusSearch/Controllers/IMuseumNewspaperController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using static CorpusSearch.Service.IMuseumNewspaperService;
+
+namespace CorpusSearch.Controllers
+{
+    [Route("[controller]")]
+    public class IMuseumNewspaperController : Controller
+    {
+        [HttpGet("Image/V1")]
+        public IActionResult Image([FromQuery] string newspaper, [FromQuery] string date, [FromQuery] string id)
+        {
+            try
+            {
+                NewspaperClippingReference reference = NewspaperClippingReference.FromOrThrow(newspaper, date, id);
+                string href = reference.Href;
+                string clipId = reference.NewspaperClippingReferenceId;
+                return base.Redirect($"https://www.imuseum.im/Olive/APA/IsleofMan/get/image.ashx?kind=block&href={href}&id={clipId}&ext=.png");
+            } 
+            catch (Exception e)
+            {
+                return base.BadRequest(e.Message);
+            }
+        }
+    }
+}
