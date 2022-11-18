7b91e2e20ea9c9dd3a51de4fb0b388aab0c22126
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-18 15:14:45 +0000

    refactor: convert FetchDataDocument to hooks

--- CorpusSearch/ClientApp/src/App.tsx
+++ CorpusSearch/ClientApp/src/App.tsx
@@ -2,7 +2,6 @@ import React, { Component } from "react"
 import {
     Route,
     Routes,
-    useLocation, useMatch
 } from "react-router-dom"
 import { Layout } from "./components/Layout"
 import {HomeFC} from "./components/Home"
@@ -17,16 +16,9 @@ export default class App extends Component {
           <Layout>
               <Routes>
                   <Route path='/' element={<HomeFC/>} />
-                  <Route path='/docs/:docId' element={<FetchDataDocumentHOC/>} />
+                  <Route path='/docs/:docId' element={<FetchDataDocument/>} />
               </Routes>
           </Layout>
     )
   }
-}
-
-const FetchDataDocumentHOC = () => {
-    const location = useLocation()
-    const match = useMatch("/docs/:docId")
-
-    return <FetchDataDocument location={location} match={match} />
 }
