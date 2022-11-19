93a695043f83d2961b5cbfa691d430658b179727
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-19 18:40:18 +0000

    Update download_cregeen.sh

--- .github/scripts/download_cregeen.sh
+++ .github/scripts/download_cregeen.sh
@@ -1,1 +1,2 @@
 curl -sL https://api.github.com/repos/david-allison/cregeen-manx-dictionary-data/releases/latest | jq -r '.assets[].browser_download_url' | xargs -n 1 curl -o cregeen.json -sSL
+echo 'downloaded'
