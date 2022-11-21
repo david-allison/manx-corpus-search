319e697b261985fc5671dde6fb8a9d363dbb390a
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-21 23:21:55 +0000

    feat!: improve dictionary summary
    * obtain 'EntryText' from next version of Cregeen
    * If it exists, use it
    * ! Return an array of objects rather than a single string of dictionary summaries
      * More flexible - but will break this again

--- CorpusSearch/ClientApp/src/api/DictionaryApi.ts
+++ CorpusSearch/ClientApp/src/api/DictionaryApi.ts
@@ -1,8 +1,9 @@
-type DictionaryResponse = string
+type Summary = { summary: string }
+export type DictionaryResponse = Summary[]
 
 export const manxDictionaryLookup = async (queryUnsafe: string): Promise<DictionaryResponse> => {
     const query = encodeURIComponent(queryUnsafe)
     const response = await fetch(`api/Dictionary/?lang=gv&word=${query}`)
     // TODO: Validation
-    return await response.text() 
+    return await response.json() 
 }
--- CorpusSearch/Service/ISearchDictionary.cs
+++ CorpusSearch/Service/ISearchDictionary.cs
@@ -5,7 +5,7 @@ namespace CorpusSearch.Service
     public interface ISearchDictionary
     {
         string Identifier { get; }
-        IEnumerable<DictionarySummary> GetSummaries(string query);
+        IEnumerable<DictionarySummary> GetSummaries(string query, bool basic = false);
     }
 
     /// <summary>When a query is made, provide a short summary of the result</summary>
