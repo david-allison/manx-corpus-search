246ec419651de49af562c58fd2cfa93ff19d8879
David Allison <62114487+david-allison@users.noreply.github.com>
2022-11-26 16:06:38 +0000

    feat: allow selection of corrected text
    Allows selection of both the 'correction' and any 'good' text in the corrected segment

--- CorpusSearch/ClientApp/src/utils/Selection.ts
+++ CorpusSearch/ClientApp/src/utils/Selection.ts
@@ -6,29 +6,72 @@ export const getSelectedWordOrPhrase = (selection: Selection) => {
     if (node == null) {
         return null
     }
+    debugger
+    const currentSelection = selection.toString()
+    const beforeSelection = setRangeStartOffset(range, node)
+    debugger
+    const afterSelection = setRangeEndOffset(range, node)
 
-    setRangeStartOffset(range, node)
-    setRangeEndOffset(range, node)
-
-    return range.toString();
+    return `${beforeSelection}${currentSelection}${afterSelection}`
 }
 
-const setRangeStartOffset = (range: Range, node: Node) => {
-    while (range.toString().indexOf(" ") != 0 && range.startOffset > 0) {
-        range.setStart(node,(range.startOffset -1))
+const setRangeStartOffset = (inputRange: Range, node: Node) => {
+    const range = inputRange.cloneRange()
+    let currentString = ""
+    while (!range.toString().includes(" ")) {
+        if (range.startOffset == 0) {
+            currentString = range.toString() + currentString
+            if (node?.parentNode?.previousSibling == null) {
+                return currentString
+            }
+            node = node.parentNode.previousSibling.childNodes[0]
+            if (node.textContent == null) {
+                console.warn("unexpected empty element")
+                return currentString
+            }
+            range.setEnd(node, node.textContent.length)
+            range.setStart(node, node.textContent.length)
+        }
+
+        while (range.toString().indexOf(" ") != 0 && range.startOffset > 0) {
+            range.setStart(node, (range.startOffset - 1))
+        }
+
+        // reached a space and we can end
+        if (range.startOffset != 0) {
+            range.setStart(node, range.startOffset + 1)
+            currentString = range.toString() + currentString
+            return currentString
+        }
     }
-    if (range.startOffset != 0 || range.toString()[0] == " ") {
-        // if we reached a space, ignore it
-        range.setStart(node, range.startOffset + 1)
-    }
+    return range.toString() + currentString
 }
 
-const setRangeEndOffset = (range: Range, node: Node) => {
-    try {
-        do {
-            range.setEnd(node,range.endOffset + 1)
-        } while(range.toString().indexOf(" ") == -1 && range.toString().trim() != "")
-    } catch (e) {
-        // TODO: find a less hacky way to end if at the end
+const setRangeEndOffset = (inputRange: Range, node: Node) => {
+    const range = inputRange.cloneRange()
+
+    let currentString = ""
+    let firstTime = true
+    while (!range.toString().includes(" ")) {
+        currentString += range.toString()
+        if (!firstTime) {
+            if (node?.parentNode?.nextSibling == null) {
+                return currentString
+            }
+            node = node.parentNode.nextSibling.childNodes[0]
+            range.setEnd(node, 0)
+            range.setStart(node, 0)
+        }
+        
+        try {
+            do {
+                range.setEnd(node,range.endOffset + 1)
+            } while(!range.toString().includes(" ") && range.toString().trim() != "")
+        } catch (e) {
+            // TODO: find a less hacky way to end if at the end
+        }
+        firstTime = false
     }
+    
+    return currentString + range.toString()
 }
