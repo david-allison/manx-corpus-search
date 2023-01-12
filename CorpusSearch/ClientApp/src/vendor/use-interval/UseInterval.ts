dee16f202919cd9dc5dce952f20849a153a5b1ef
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-12 03:32:08 +0000

    vendor: donavon/use-interval
    No need for a hard dependency here - vendor the code

---
+++ CorpusSearch/ClientApp/src/vendor/use-interval/UseInterval.ts
@@ -0,0 +1,56 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019-present Donavon West
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * https://github.com/donavon/use-interval/blob/59d07e435f20e2116b489bb7dc211eff332f327e/src/index.tsx
+ */
+
+import { useEffect, useRef } from 'react';
+
+type Delay = number | null;
+type TimerHandler = (...args: any[]) => void;
+
+/**
+ * Provides a declarative useInterval
+ *
+ * @param callback - Function that will be called every `delay` ms.
+ * @param delay - Number representing the delay in ms. Set to `null` to "pause" the interval.
+ */
+
+const useInterval = (callback: TimerHandler, delay: Delay) => {
+    const savedCallbackRef = useRef<TimerHandler>();
+
+    useEffect(() => {
+        savedCallbackRef.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+        const handler = (...args: any[]) => savedCallbackRef.current!(...args);
+
+        if (delay !== null) {
+            const intervalId = setInterval(handler, delay);
+            return () => clearInterval(intervalId);
+        }
+    }, [delay]);
+};
+
+export default useInterval;
