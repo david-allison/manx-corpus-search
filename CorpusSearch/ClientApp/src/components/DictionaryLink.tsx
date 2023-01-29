2888874560ca613b5434308261f22aaf2d1c597d
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-29 15:17:03 +0000

    dictionary: bold numbers
    and refactor

--- CorpusSearch/ClientApp/src/components/DictionaryLink.tsx
+++ CorpusSearch/ClientApp/src/components/DictionaryLink.tsx
@@ -1,15 +1,39 @@
 import React from "react"
-import {DefinedInDictionaries} from "../api/SearchApi"
+import {DefinedInDictionaries, DictionaryDefinition} from "../api/SearchApi"
 
 export const DictionaryLink = (props: { query: string, dictionaries: DefinedInDictionaries}) => {
-
         return <>
-            {Object.keys(props.dictionaries).map(dictionaryName => <>
-                    {props.dictionaries[dictionaryName].allowLookup ? 
-                        <a href={`/Dictionary/${dictionaryName}/${props.query}`} target="_blank" rel="noreferrer" style={{ "fontWeight": "bold" }}>{dictionaryName}</a> :
-                        <span style={{ "fontWeight": "bold" }}>{dictionaryName}</span> }
-                    : {props.dictionaries[dictionaryName].entries.map((e, i) => `${i+1}) ${e}`).join("; ")}<br />
-                </>)}
+            {Object.keys(props.dictionaries).map(dictionaryName => {
+                const element = props.dictionaries[dictionaryName]
+                return (
+                    <>
+                        <DictionaryNameHeader dictionary={element} dictionaryName={dictionaryName} query={props.query} />
+                        : {/*: A literal colon*/}
+                        {
+                            element.entries.map((e, i) => 
+                            <>
+                                <b>{`${i+1})`}</b>
+                                {` ${e}`}
+                                {i == element.entries.length -1 ? "" : "; "}
+                            </>)
+                        }
+                        <br />
+                    </>
+                )})
+            }
         </>
     
+}
+
+const DictionaryNameHeader = (props: { dictionary: DictionaryDefinition, dictionaryName: string, query: string}) => {
+        const {dictionaryName, query, dictionary } = props
+    
+    if (!dictionary.allowLookup) {
+        return <span style={{ "fontWeight": "bold" }}>{dictionaryName}</span>
+    }
+    
+    return <a href={`/Dictionary/${dictionaryName}/${query}`} target="_blank" rel="noreferrer" style={{ "fontWeight": "bold" }}>
+                {dictionaryName}
+            </a>
+            
 }
