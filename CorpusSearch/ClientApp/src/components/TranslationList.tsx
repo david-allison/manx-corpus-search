59c503fbe797862ded44443a165e481c5f5b8228
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-29 15:25:53 +0000

    improve vertical spacing of dictionaries

--- CorpusSearch/ClientApp/src/components/TranslationList.tsx
+++ CorpusSearch/ClientApp/src/components/TranslationList.tsx
@@ -1,10 +1,20 @@
 import React from "react"
 import {Translations} from "../api/SearchApi"
 
+export function hasTranslations(translations?: Translations) {
+    if (!translations) { return false }
+    return Object.keys(translations).filter(code => translations[code].length > 0).length > 0
+}
 
 export const TranslationList = (props: { translations: Translations }) => {
+    const { translations } = props
     return <>
-        {Object.keys(props.translations).map(langCode => <><strong>{langCode}:</strong> {props.translations[langCode].map(x => <>{x}, </>)}
-        </>)}
+        {Object.keys(translations).map(langCode => {
+            const langCodeTranslations = translations[langCode]
+            if (langCodeTranslations.length == 0) { return <></> }
+            return <><strong>{langCode}:</strong> {langCodeTranslations.map(x => <>{x}, </>)}</>}
+        )}
+        {/*return a newline if we had results*/}
+        <br/>
     </>
 }
