b2ae862f8d84d4034a67e8e116fb2f66511af5d1
David Allison <62114487+david-allison@users.noreply.github.com>
2023-01-29 16:55:29 +0000

    Allow user to clear search bar

--- CorpusSearch/ClientApp/src/components/SearchBar.tsx
+++ CorpusSearch/ClientApp/src/components/SearchBar.tsx
@@ -2,6 +2,6 @@ import React, {ChangeEventHandler} from "react"
 
 export const SearchBar = (props: {query: string, onChange :ChangeEventHandler<HTMLInputElement>}) => {
     const {query, onChange} = props
-    return <input size={5} id="corpus-search-box" style={{flexGrow: 1, marginLeft: 12}} placeholder="Enter search term" type="text" value={query} onChange={onChange} />
+    return <input size={5} id="corpus-search-box" style={{flexGrow: 1, marginLeft: 12}} placeholder="Enter search term" type="search" value={query} onChange={onChange} />
 
 }
