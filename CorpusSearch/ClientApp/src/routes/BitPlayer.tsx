d3f85c77cb01d3a0a5eb0633750d1f9e831ef50d
lauterb <47546076+lauterb@users.noreply.github.com>
2024-04-13 19:31:56 -0400

    feat: loop YouTube videos to assist in timestamping (#193)
    * Buggy initial attempt at looping
    
    * Use useInterval instead of SetInterval
    And make some names a little more readable
    
    * put useInterval inside eslint-disable, remove semicolons

--- CorpusSearch/ClientApp/src/routes/BitPlayer.tsx
+++ CorpusSearch/ClientApp/src/routes/BitPlayer.tsx
@@ -5,6 +5,7 @@ since this is kind of a no-frills YouTube player*/
 import "./DocumentView.css"
 import React, {useRef, useState} from "react"
 import YouTube, {YouTubePlayer, YouTubeProps} from "react-youtube"
+import useInterval from "../vendor/use-interval/UseInterval"
 
 function Video({vId="rLEBp8R1_XA"}) {
     const r_player = useRef<YouTubePlayer>(null)
@@ -14,9 +15,22 @@ function Video({vId="rLEBp8R1_XA"}) {
     const [ktime, setKtime] = useState(-1)
     
     const [video, setVideoId] = useState(vId)
-    const [stime, setStime  ] = useState("50.00")
+    const [startTime, setStartTime  ] = useState("50.00")
+    const [endTime, setEndTime] = useState("60.00")
 
-/* eslint-disable @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+    const [loop, setLoop] = useState(false)
+
+    /* eslint-disable @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+    useInterval(() => {
+        if (!loop) return
+        const vt = r_player.current.getCurrentTime()
+        const endTimeAsFloat = parseFloat(endTime)
+        if (!endTimeAsFloat || endTimeAsFloat >= dur) return
+        if (vt >= endTimeAsFloat) {
+            restart()
+        }
+    }, 10)
+
     const onPlay: YouTubeProps["onPlay"] = (event) => {
         setTime0(event.target.getCurrentTime())
         setPtime(-1)
@@ -33,25 +47,31 @@ function Video({vId="rLEBp8R1_XA"}) {
         setDur(the_player.getDuration())
     }
     const onSChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setStime(e.target.value)
-    } 
+        setStartTime(e.target.value)
+    }
+    const onEChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEndTime(e.target.value)
+    }
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setVideoId(e.target.value)
     }
     const restart = () => {
-        r_player.current.seekTo(parseFloat(stime))
+        r_player.current.seekTo(parseFloat(startTime))
         r_player.current.playVideo()
     }
     const onClick = (/*e :MouseEvent<HTMLButtonElement>*/) => {
         setKtime(r_player.current.getCurrentTime())
         restart()
     }
+
     /* eslint-enable */
     return <>
         <div><YouTube videoId={video} onReady={onReady} onPlay = {onPlay} onPause={onPause}/></div>
         <div> Video ID: <input type={"text"} value={video} onChange={onChange}/></div>
-        <div> Seek to: <input type={"number"} step={"0.01"} value={stime} onChange={onSChange}/></div>
+        <div> Loop Start: <input type={"number"} step={"0.01"} value={startTime} onChange={onSChange}/></div>
+        <div> Loop End: <input type={"number"} step={"0.01"} value={endTime} onChange={onEChange}/></div>
         <button onClick={onClick}>Click To Seek and Play</button>
+        <div> Loop <input type="checkbox" checked={loop} onChange={() => setLoop(x => !x)}/></div>
         {dur > 0 ? <div>Duration: {dur}</div> : ""}
         {ktime >= 0 && <div>Seek  from: {ktime.toFixed(2)}</div>}
         {time0 >= 0 && <div>Started at: {time0.toFixed(2)}</div>}
