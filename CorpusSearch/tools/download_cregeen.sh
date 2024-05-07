8f0227a8bece0c5355ccfc6f74b2279928eb8d91
David Allison <62114487+david-allison@users.noreply.github.com>
2024-05-07 20:25:39 +0100

    build: setup docker build
    * use /tools/ to ensure they're visible to the Dockerfile
    
    An init script handles ensuring that the data is up to date

---
+++ CorpusSearch/tools/download_cregeen.sh
@@ -0,0 +1,3 @@
+echo 'Downloading Cregeen...'
+curl -sL https://api.github.com/repos/david-allison/cregeen-manx-dictionary-data/releases/latest | jq -r '.assets[].browser_download_url' | xargs -n 1 curl -o cregeen.json -sSL
+
