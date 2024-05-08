77b0f58093e4afaa92517441cd5a115bb6ea7c68
David Allison <62114487+david-allison@users.noreply.github.com>
2024-05-08 16:58:36 +0100

    chore(lint): replace if with 'return'

--- CorpusSearch/Model/CorpusSearchWorkQuery.cs
+++ CorpusSearch/Model/CorpusSearchWorkQuery.cs
@@ -15,11 +15,6 @@ public class CorpusSearchWorkQuery(string query)
             return false;
         }
 
-        if (!Manx && !English)
-        {
-            return false;
-        }
-
-        return true;
+        return Manx || English;
     }
 }
