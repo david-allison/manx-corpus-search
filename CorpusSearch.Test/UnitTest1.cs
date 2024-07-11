12ec105909896bfccfbbc3ae81419ef7b7f19d3a
David Allison <62114487+david-allison@users.noreply.github.com>
2024-07-11 22:18:25 +0100

    NuGet: Update packages in solution
    EXCEPT CsvHelper - compile changes required

--- CorpusSearch.Test/UnitTest1.cs
+++ CorpusSearch.Test/UnitTest1.cs
@@ -25,11 +25,11 @@ public class Tests
         var squote = "\u2018\u2019\u201b\u2032";
         var dquote = "\u201c\u201d\u201e\u2033";
         var ellipsis = "\u2026";
-        Assert.AreEqual(new string('-', dash.Length), DocumentLine.NormalizeManx(dash));
-        Assert.AreEqual(new string('\'', squote.Length), DocumentLine.NormalizeManx(squote));
-        Assert.AreEqual(string.Empty, DocumentLine.NormalizeManx(dquote));
-        Assert.AreEqual("_", DocumentLine.NormalizeManx(underbar));
-        Assert.AreEqual(",", DocumentLine.NormalizeManx(comma));
-        Assert.AreEqual("...", DocumentLine.NormalizeManx(ellipsis));
+        Assert.That(new string('-', dash.Length), Is.EqualTo(DocumentLine.NormalizeManx(dash)));
+        Assert.That(new string('\'', squote.Length), Is.EqualTo(DocumentLine.NormalizeManx(squote)));
+        Assert.That(string.Empty, Is.EqualTo(DocumentLine.NormalizeManx(dquote)));
+        Assert.That("_", Is.EqualTo(DocumentLine.NormalizeManx(underbar)));
+        Assert.That(",", Is.EqualTo(DocumentLine.NormalizeManx(comma)));
+        Assert.That("...", Is.EqualTo(DocumentLine.NormalizeManx(ellipsis)));
     }
 }
