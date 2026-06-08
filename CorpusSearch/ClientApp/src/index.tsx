2993f20ac79b2de2ea22de3958899d7024989485
David Allison <62114487+david-allison@users.noreply.github.com>
2026-06-08 16:21:41 +0100

    feat(web): remove PWA-related code
    the app is a search (effectively online-only), so this is not useful

--- CorpusSearch/ClientApp/src/index.tsx
+++ CorpusSearch/ClientApp/src/index.tsx
@@ -2,7 +2,6 @@ import "bootstrap/dist/css/bootstrap.css"
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") ?? undefined
 const rootElement = document.getElementById("root")
@@ -12,6 +11,4 @@ root.render(
     <BrowserRouter basename={baseUrl}>
     <App />
 </BrowserRouter>);
-
-serviceWorkerRegistration.unregister()
 
