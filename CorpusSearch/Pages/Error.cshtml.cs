13c1ad420878e64c615d1ffd895703a5043c8c32
David Allison <62114487+david-allison@users.noreply.github.com>
2026-07-11 21:49:33 +0100

    lint: IDE0051/0052: unused/unread private member

--- CorpusSearch/Pages/Error.cshtml.cs
+++ CorpusSearch/Pages/Error.cshtml.cs
@@ -1,21 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace CorpusSearch.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-public class ErrorModel(ILogger<ErrorModel> logger) : PageModel
+public class ErrorModel : PageModel
 {
-    private readonly ILogger<ErrorModel> _logger = logger;
-
     public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet()
     {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
 }
